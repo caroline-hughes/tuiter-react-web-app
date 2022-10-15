dedeefc29d6640f11cd70294a9f8c0ee57149f15
carolinehughes1717@gmail.com <carolinehughes1717@gmail.com>
2022-10-15 08:20:04 -0700

    done

--- public/tuiter/ExploreComponent/index.js
+++ public/tuiter/ExploreComponent/index.js
@@ -2,46 +2,43 @@ import PostSummaryList from "../PostSummaryList";
 
 const ExploreComponent = () => {
     return(`
-    <div class="row">
-
-                <div class="col-11">
-                    <div class="input-group mb-3 rounded-pill">
-                        <div class="input-group-prepend m-2">
-                            <i class="fa-solid fa-magnifying-glass"></i>
-                        </div>
-                        <input type="text" class="form-control" placeholder="Search Twitter"/>
+        <div class="row">
+            <div class="col-11">
+                <div class="input-group mb-3 rounded-pill bg-color-white">
+                    <div class="input-group-prepend m-2">
+                        <i class="fa-solid fa-magnifying-glass"></i>
                     </div>
+                    <input type="text" class="form-control" placeholder="Search Twitter"/>
                 </div>
-                <div class="col-1 mt-1">
-                    <a href="#" > <i class="fa-solid fa-gear fa-2x primary"></i> </a>
-                </div>
-
             </div>
-
-            <ul class="nav nav-tabs mt-2">
-                <li class="nav-item">
-                    <a href="for-you.html" class="nav-link active">For You</a>
-                </li>
-                <li class="nav-item">
-                    <a href="trending.html" class="nav-link">Trending</a>
-                </li>
-                <li class="nav-item">
-                    <a href="news.html" class="nav-link">News</a>
-                </li>
-                <li class="nav-item">
-                    <a href="sports.html" class="nav-link">Sports</a>
-                </li>
-                <li class="nav-item d-none d-lg-block">
-                    <a href="entertainment.html" class="nav-link">Entertainment</a>
-                </li>
-            </ul>
-
-            <div class="article-title">
-                <img src="../images/spaceship.jpg" class="img-responsive">
-                <span class="overlay-text text-xl-left fw-bold">SpaceX's Starship</span>
+            <div class="col-1 mt-1">
+                <a href="#" > <i class="fa-solid fa-gear fa-2x primary"></i> </a>
             </div>
-           ${PostSummaryList()}
-    </div>
+        </div>
+
+        <ul class="nav nav-tabs mt-2">
+            <li class="nav-item">
+                <a href="for-you.html" class="nav-link active">For You</a>
+            </li>
+            <li class="nav-item">
+                <a href="trending.html" class="nav-link">Trending</a>
+            </li>
+            <li class="nav-item">
+                <a href="news.html" class="nav-link">News</a>
+            </li>
+            <li class="nav-item">
+                <a href="sports.html" class="nav-link">Sports</a>
+            </li>
+            <li class="nav-item d-none d-lg-block">
+                <a href="entertainment.html" class="nav-link">Entertainment</a>
+            </li>
+        </ul>
+
+        <div class="article-title">
+            <img src="../images/spaceship.jpg" class="img-responsive">
+            <span class="overlay-text text-xl-left fw-bold">SpaceX's Starship</span>
+        </div>
+        ${PostSummaryList()}
 `);
 }
 export default ExploreComponent;
--- public/tuiter/explore/index.js
+++ public/tuiter/explore/index.js
@@ -5,16 +5,29 @@ import ExploreComponent from "../ExploreComponent";
 function exploreComponent() {
     $('#wd-explore').append(`
   <div class="row mt-2">
+  
        <div class="col-2 col-md-2 col-lg-1 col-xl-2">
         ${NavigationSidebar()}
        </div>
-       <div class="col-10 col-lg-7 col-xl-6 text-white">
+       
+        <div class="col-10 col-lg-7 col-xl-6 text-white">
         ${ExploreComponent()}
        </div>
+       
        <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4 text-white">
         ${WhoToFollowList()}
        </div>
+       
+      
+       
+       
+       
+       
   </div>
    `);
 }
 $(exploreComponent);
+
+// <div className="col-xxl-2 col-xl-2 col-lg-1 col-md-2 col-sm-2">
+// <div className="col-xxl-6 col-xl-6 col-lg-8 col-md-10 col-sm-10">
+// <div className="col-xxl-4 col-xl-4 col-lg-3 d-none d-lg-block">
